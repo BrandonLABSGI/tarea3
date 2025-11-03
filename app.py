4c584f6ee39cc547e9fbbb6f1739ed97765bde66
BrandonLABSGI <00202521@uca.edu.sv>
2025-11-02 22:13:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,12 @@ import streamlit as st
 from modulos.venta import mostrar_venta
 from modulos.login import login
 #from modulos.compra import mostrar_compra
-mostrar_venta()
-login()
+#mostrar_venta()
+#login()
+# Comprobamos si la sesión ya está iniciada
+if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
+    # Si la sesión está iniciada, mostrar el contenido de ventas
+    mostrar_venta()
+else:
+    # Si la sesión no está iniciada, mostrar el login
+    login()
